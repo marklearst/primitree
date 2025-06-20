17f07ba136d0892e0278ba60483fd7281c8ed39f
Mark Learst <460323+marklearst@users.noreply.github.com>
2025-06-20 13:53:49 -0400

    refactor(constants): Improve organization and reduce redundancy

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,11 +5,14 @@ export const FIGMA_API_BASE_URL = 'https://api.figma.com'
 
 // Specific API endpoints
 export const FIGMA_FILES_ENDPOINT = `${FIGMA_API_BASE_URL}/v1/files`
+
+const FIGMA_V1_VARIABLES_ENDPOINT_BASE = `${FIGMA_API_BASE_URL}/v1/variables`
+
 export const FIGMA_VARIABLES_ENDPOINT = (fileKey: string) =>
   `${FIGMA_FILES_ENDPOINT}/${fileKey}/variables`
-export const FIGMA_POST_VARIABLES_ENDPOINT = `${FIGMA_API_BASE_URL}/v1/variables`
+export const FIGMA_POST_VARIABLES_ENDPOINT = FIGMA_V1_VARIABLES_ENDPOINT_BASE
 export const FIGMA_VARIABLE_BY_ID_ENDPOINT = (variableId: string) =>
-  `${FIGMA_API_BASE_URL}/v1/variables/${variableId}`
+  `${FIGMA_V1_VARIABLES_ENDPOINT_BASE}/${variableId}`
 export const FIGMA_LOCAL_VARIABLES_ENDPOINT = (fileKey: string) =>
   `${FIGMA_FILES_ENDPOINT}/${fileKey}/variables/local`
 
@@ -18,9 +21,11 @@ export const CONTENT_TYPE_JSON = 'application/json'
 export const FIGMA_TOKEN_HEADER = 'X-FIGMA-TOKEN'
 
 // Error Messages
+// -- Auth Errors
 export const ERROR_MSG_TOKEN_REQUIRED = 'A Figma API token is required.'
-export const ERROR_MSG_TOKEN_FILE_KEY_REQUIRED =
-  'A Figma API token and file key are required.'
+export const ERROR_MSG_TOKEN_FILE_KEY_REQUIRED = `${ERROR_MSG_TOKEN_REQUIRED} and file key are required.`
+
+// -- Mutation Errors
 export const ERROR_MSG_BULK_UPDATE_FAILED = 'Failed to perform bulk update.'
 export const ERROR_MSG_CREATE_VARIABLE_FAILED =
   'Failed to create Figma variable.'
@@ -28,5 +33,7 @@ export const ERROR_MSG_DELETE_VARIABLE_FAILED =
   'Failed to delete Figma variable.'
 export const ERROR_MSG_UPDATE_VARIABLE_FAILED =
   'Failed to update Figma variable.'
+
+// -- Fetch Errors
 export const ERROR_MSG_FETCH_FIGMA_DATA_FAILED =
   'An error occurred while fetching data from the Figma API.'
