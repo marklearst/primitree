1f8d4e4699347a786152eef54fdfd57c39881016
Mark Learst <460323+marklearst@users.noreply.github.com>
2025-06-22 21:19:43 -0400

    fix(test): configure test environments to load .env variables

--- wallaby.js
+++ wallaby.js
@@ -1,6 +1,12 @@
-module.exports = function () {
+module.exports = function (wallaby) {
   return {
-    files: ['src/**/*', 'tests/**/*', '!**/*.test.*'],
+    files: [
+      'src/**/*',
+      'tests/**/*',
+      '.env',
+      'vitest.setup.ts',
+      '!**/*.test.*',
+    ],
     tests: ['tests/**/*.test.*', 'src/**/*.test.*'],
 
     // Tell Wallaby it's a Vitest project and to use the Vite config.
@@ -10,6 +16,7 @@ module.exports = function () {
     },
 
     setup: (wallaby) => {
+      require('dotenv').config()
       const vitestConfig = require('./vite.config.ts').default.test
       wallaby.testFramework.setupFiles = vitestConfig.setupFiles
     },
